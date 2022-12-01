ad33eebcb28de86151f8b119a7a6510eac579011
thelenaza <kaxtie07@gmail.com>
2022-12-01 17:37:10 +0700

    update

--- week701.php
+++ week701.php
@@ -12,9 +12,17 @@
         return $this->c;
       }
    }
+
    $obj1 = new Calculate(); //objact
+   $obj2 = new Calculate();
+
+
    $obj1->a = 20;
    $obj1->b = 10;
-   print($obj1->sum());
+   print("Objact1 = ".$obj1->sum())."<br />";
 
+
+   $obj2->a = 50;
+   $obj2->b = 30;
+   print("Objact2 = ".$obj2->sub());
 ?>
